ab5314d624d029068703549eadd1009e5e69f9c4
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-04-09 21:58:27 -0500

    Handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./home');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars')
+
 const app = express();
+const routes = require('./controllers');
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
 
 app.listen(PORT, () => { console.log(`Listening on ${PORT}`)});
