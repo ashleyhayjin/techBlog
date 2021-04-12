38f968c57da8fee2fe2af39654a009b13b3d1912
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-04-11 21:34:44 -0500

    Session

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -23,17 +23,23 @@ const { User, Post } = require('../../models');
 // });
 
 router.post('/', async (req, res) => {
-  if (req.session.loggedIn)
   try {
-    const newPost = await Post.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    console.log(req.body);
+    const userData = await User.create(
+      {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
     });
-
-    res.status(200).json(newPost);
-  } catch (err) {
-    res.status(400).json(err);
+    req.session.save(() => {
+      req.session.username = userData.username;
+      req.session.email= userData.email;
+      req.session.password = userData.password;
+      req.session.logged_in = true;
+      res.status(200).json(userData);
+    });   
+    } catch (err) {
+      res.status(400).json(err);
   }
 });
-
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post} = require('../models');
-
+const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
 
 
 router.get('/login', async (req,res) => {
-    if (req.session.cookie.loggedIn){
+    if (req.session.loggedIn){
         res.redirect('/');
         return;
     }
